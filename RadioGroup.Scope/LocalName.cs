c4eec2740bb39f56fb007ceedfe53aa281a6cb87
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-09-03 17:20:33 -0500

    Commit

---
+++ RadioGroup.Scope/LocalName.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace RadioGroup.Scope
+{
+    public class LocalName
+    {
+        public static string GetBaseName(FrameworkElement obj)
+        {
+            return (string)obj.GetValue(BaseNameProperty);
+        }
+
+        public static void SetBaseName(FrameworkElement obj, string value)
+        {
+            obj.SetValue(BaseNameProperty, value);
+        }
+
+        public static readonly DependencyProperty BaseNameProperty =
+            DependencyProperty.RegisterAttached("BaseName", typeof(string),
+            typeof(LocalName),
+            new FrameworkPropertyMetadata(null,
+                FrameworkPropertyMetadataOptions.Inherits));
+    }
+}
---
+++ RadioGroup.Scope/UniqueNameExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace RadioGroup.Scope
+{
+    public class UniqueNameExtension : MarkupExtension
+    {
+        private string _name;
+
+        public UniqueNameExtension()
+        {
+            _name = Guid.NewGuid().ToString("N");
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _name;
+        }
+    }
+}
